713669e2326ad2f9529ba5ce77c3434de24256a3
VaibhaviPatil-86868 <vaibhavivkp@gmail.com>
2024-12-17 16:00:14 +0530

    Express app1

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const express=require('express')
-const app=express();
-app.get('/',(req,res)=>{
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
     res.send("Hello");
-})
-app.listen(3000,'0.0.0.0',()=>{
-    console.log("Welcome to my world...")
-})
+});
+
+app.listen(3000, '0.0.0.0', () => {
+    console.log("Welcome to my world...");
+});
